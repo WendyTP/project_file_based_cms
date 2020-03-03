629e0096321986e87a637db70a27e769952c763e
Wendy <wendytsai1988@gmail.com>
2020-03-03 12:13:08 +0100

    add index page for files in data directory

--- cms.rb
+++ cms.rb
@@ -1,6 +1,14 @@
 require "sinatra"
 require "sinatra/reloader"
+require "tilt/erubis"
+
+root = File.expand_path("..", __FILE__)
+
 
 get "/" do
-  "Getting started"
+  @files = Dir.glob(root + "/data/*").map do |path|
+    File.basename(path)
+  end
+
+  erb :index
 end
