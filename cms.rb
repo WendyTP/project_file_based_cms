c8887716636841f09be36ebde53123e15ac53927
Wendy <wendytsai1988@gmail.com>
2020-03-04 11:01:35 +0100

    viewing markdown files

--- cms.rb
+++ cms.rb
@@ -1,18 +1,38 @@
+
 require "sinatra"
 require "sinatra/reloader"
 require "tilt/erubis"
+require "redcarpet"
+
 
 configure do
   enable :sessions
   set :session_secret, "secret"
 end
 
+
+def render_markdown(text)
+  markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
+  markdown.render(text)
+end
+
+def load_file_content(file)
+  file_content = File.read(file)
+
+  case File.extname(file)
+  when ".txt"
+    headers["Content-Type"] = "text/plain"
+    file_content
+  when ".md"
+    render_markdown(file_content)
+  end
+end
+
+ 
 root = File.expand_path("..", __FILE__)
 
-
-
 get "/" do
-  @files = Dir.glob(root + "/data/*").map do |path|
+  @files =  Dir.glob(root + "/data/*").map do |path|
     File.basename(path)
   end
 
@@ -20,18 +40,12 @@ get "/" do
 end
 
 get "/:filename" do
-  @files = Dir.glob(root + "/data/*").map do |path|
-    File.basename(path)
-  end
+  file_path = "#{root}" + "/data/#{params[:filename]}"
 
-  if @files.include?(params[:filename])
-    headers["Content-Type"] = "text/plain"
-
-    file_path = "#{root}" + "/data/#{params[:filename]}"
-    File.read(file_path)
+  if File.exist?(file_path)
+    load_file_content(file_path)
   else
     session[:error] = "#{params[:filename]} does not exist."
     redirect "/"
   end
-
 end
--- test/cms_test.rb
+++ test/cms_test.rb
@@ -16,7 +16,7 @@ class CmsTest < Minitest::Test
     get "/"
     assert_equal(200, last_response.status)
     assert_equal("text/html;charset=utf-8", last_response["Content-Type"])
-    assert_includes(last_response.body, "about.txt")
+    assert_includes(last_response.body, "about.md")
     assert_includes(last_response.body, "changes.txt")
     assert_includes(last_response.body, "history.txt")
   end
@@ -39,5 +39,12 @@ class CmsTest < Minitest::Test
     get "/"
     assert_equal(200, last_response.status)
     refute_includes(last_response.body, "notafile.txt does not exist.")
+  end
+
+  def test_markdown_render_to_html
+    get "/about.md"
+    assert_equal(200, last_response.status)
+    assert_equal("text/html;charset=utf-8", last_response["Content-Type"])
+    assert_includes(last_response.body, "<h1>Ruby is...</h1>" )
   end
 end
